f85561320c5bb995f9a6c87939892b915aef1e1f
Yosvanis Posada Garcia <yosvanis.posada@gmail.com>
2024-04-14 17:21:07 -0400

    Variable de entorno intervalo agregada port

--- bot.js
+++ bot.js
@@ -11,6 +11,16 @@ const intervalo = process.env.TELEGRAM_INTERVALO;
 const sessionData = new Map();
 const bot = new TelegramBot(token, { polling: true });
 
+const express = require("express");
+const app = express();
+
+// This line is important to ensure your app listens to the PORT env var
+const port = process.env.PORT ?? 8080;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
 let automaticMode = false;
 let automaticModeParams = {};
 
